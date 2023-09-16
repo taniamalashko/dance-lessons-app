457f3280b9767ae4e124dee4d039f734d36648b7
taniamalashko <sashaand.tanya8@gmail.com>
2023-09-16 15:51:11 +0200

    class-components

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,94 +1,115 @@
 import { lessons } from "../../api/lessons/lessons";
-import React, { useState } from "react";
-import { Button, FavoriteButton, InfoButton, InfoContainer, InfoContainerButtons, InfoContainerName, VideoCard, VideoCardContainer, VideoPreview } from "./cardStyled";
+import React, { Component } from "react";
+import { 
+  Button, 
+  FavoriteButton, 
+  InfoButton, 
+  InfoContainer, 
+  InfoContainerButtons, 
+  InfoContainerName, 
+  VideoCard, 
+  VideoCardContainer, 
+  VideoPreview 
+} from "./cardStyled";
 import ModalComponent from "../modal/Modal";
 
+export default class Card extends Component{
+  state = {
+    isModalOpen: false,
+  }
 
-// Function to extract the video ID from a YouTube link
-function getVideoIdFromLink(link) {
-  const url = new URL(link);
-  const searchParams = new URLSearchParams(url.search);
-  return searchParams.get("v");
-}
+  constructor(){
+    super();
 
-// Function to update the favorite status of a lesson in a list
-function updateLessonFavoriteInList(list, key, newFavoriteValue){
-  return list.map(lesson => {
-    if (lesson.id === key){
-      return {...lesson, favorite: newFavoriteValue}
-    }
-    return lesson
-  })
-}
+    this.updateLessonFavoriteInList = this.updateLessonFavoriteInList.bind(this);
+    this.getVideoIdFromLink = this.getVideoIdFromLink.bind(this);
+    this.openLessonLinkInNewTab = this.openLessonLinkInNewTab.bind(this);
+    this.updateFavorite = this.updateFavorite.bind(this);
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
 
-const Card = ({ lesson, color, updateListsFunc, originalList, lessonsList }) => {
-  const { id, name, link, favorite: lessonFavorite } = lesson;
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  getVideoIdFromLink(link){
+    const url = new URL(link);
+    const searchParams = new URLSearchParams(url.search);
+    return searchParams.get("v");
+  }
 
-  const opts = {
-    height: "200px",  
-    width: "100%",  
-    playerVars: {
-      autoplay: 0,  
-      controls: 1,  
-      modestbranding: 1,  
-      rel: 0,  
-      showinfo: 1,  
-    },
-  };
+  updateLessonFavoriteInList(list, key, newFavoriteValue){
+    return list.map(lesson => {
+      if (lesson.id === key){
+        return {...lesson, favorite: newFavoriteValue}
+      }
+      return lesson
+    })
+  }
 
-  // Function to update the favorite status of a lesson
-  async function updateFavorite(newFavoriteValue){
+  async updateFavorite(newFavoriteValue){
+
+    const { lesson, updateListsFunc, originalList, lessonsList } = this.props;
+    const { id } = lesson;
+
     try {
-      // Update the lesson's favorite status using the lessons API
       await lessons.put(id, { favorite: newFavoriteValue });
 
-      // Update the original and lessons list with the new favorite status
-      const updatedOriginalList = updateLessonFavoriteInList(originalList, id, newFavoriteValue);
-      const updatedLessonsList = updateLessonFavoriteInList(lessonsList, id, newFavoriteValue);
+      const updatedOriginalList = this.updateLessonFavoriteInList(originalList, id, newFavoriteValue);
+      const updatedLessonsList = this.updateLessonFavoriteInList(lessonsList, id, newFavoriteValue);
 
-      // Call the parent component's update function
       updateListsFunc(updatedOriginalList, updatedLessonsList);
     } catch(err) {
       console.log(err);
     }
   }
 
-  // Function to open the lesson link in a new tab
-  const openLessonLinkInNewTab = () => {
-    window.open(link, '_blank');
+  openLessonLinkInNewTab = () => {
+    const { lesson } = this.props;
+    window.open(lesson.link, '_blank');
+  }
+
+  openModal = () => {
+    this.setState({ isModalOpen: true });
   };
 
-  // Function to open the modal for more information about the lesson
-  const openModal = () => {
-    setIsModalOpen(true);
+  closeModal = () => {
+    this.setState({ isModalOpen: false });
   };
 
-  // Function to close the modal
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
+  render(){
+    const { color, lesson } = this.props;
+    const { isModalOpen } = this.state;
+    const { name, favorite: lessonFavorite } = lesson;
+    
+    const opts = {
+      height: "200px",  
+      width: "100%",  
+      playerVars: {
+        autoplay: 0,  
+        controls: 1,  
+        modestbranding: 1,  
+        rel: 0,  
+        showinfo: 1,  
+      },
+    };
 
-  return (
-    <VideoCardContainer>
+    return (
+      <VideoCardContainer>
         <VideoCard>
-          <VideoPreview videoId={getVideoIdFromLink(link)} opts={opts}/>
+          <VideoPreview videoId={this.getVideoIdFromLink(lesson.link)} opts={opts} />
           <InfoContainer color={color}>
             <InfoContainerName title={name}>
-             {name.length > 30 ? name.substring(0, 30) + '...' : name}
+              {name.length > 30 ? name.substring(0, 30) + '...' : name}
             </InfoContainerName>
             <InfoContainerButtons>
-              <Button><InfoButton onClick={openModal}>more_horiz</InfoButton></Button>
-              <Button><InfoButton onClick={openLessonLinkInNewTab}>play_circle</InfoButton></Button>
-              <Button><FavoriteButton favorite={lessonFavorite} onClick={() => updateFavorite(!lessonFavorite)}>favorite</FavoriteButton></Button>
+              <Button><InfoButton onClick={() => {this.openModal()}}>more_horiz</InfoButton></Button>
+              <Button><InfoButton onClick={this.openLessonLinkInNewTab}>play_circle</InfoButton></Button>
+              <Button><FavoriteButton favorite={lessonFavorite} onClick={() => this.updateFavorite(!lessonFavorite)}>favorite</FavoriteButton></Button>
             </InfoContainerButtons>
           </InfoContainer>
         </VideoCard>
         {isModalOpen && (
-          <ModalComponent isOpen={isModalOpen} onRequestClose={closeModal} lesson={lesson} />
-    )}
-    </VideoCardContainer>
-  );
-};
-
-export default Card;
+          <ModalComponent isOpen={isModalOpen} onRequestClose={this.closeModal} lesson={lesson} />
+        )}
+      </VideoCardContainer>
+    );
+  }
+}
--- src/pages/lessons/Lessons.jsx
+++ src/pages/lessons/Lessons.jsx
@@ -1,97 +1,103 @@
-import React, { useCallback, useEffect, useState } from "react";
+import { Component } from "react";
 import { lessons } from "../../api/lessons/lessons";
 import Card from "../../components/card/Card";
-import { Cards, ErrorBox, ErrorContainer, ErrorImg, ErrorText, FilterBar, FilterButt, LoadingContainer } from "./lessonsStyled";
+import { 
+    Cards, 
+    ErrorBox, 
+    ErrorContainer, 
+    ErrorImg, 
+    ErrorText, 
+    FilterBar, 
+    FilterButt, 
+    LoadingContainer 
+} from "./lessonsStyled";
 import { DotLoader } from "react-spinners";
 import sad from "../../assets/sad.png"
 
-export default function Lessons(){
-    // Define background colors for the cards
-    const backgroundColors = ['rgb(243, 243, 136)', 'rgb(241, 201, 208)', 'rgb(178, 178, 222)', 'rgb(178, 222, 184)'];
+export default class Lessons extends Component {
+    state = {
+        lessonsList: [],
+        originalList: [],
+        loading: true,
+        error: '',
+    }
 
-    // State variables to manage lessons list, original list, loading state, and error
-    const [ lessonsList, setLessonsList ] = useState([]);
-    const [ originalList, setOriginalList ] = useState([]);
-    const [ loading, setLoading ] = useState(true);
-    const [ error, setError ] = useState('');
+    constructor(){
+        super();
 
-    // Function to fetch lessons data from an API
-    const fetchLessons = useCallback(async () => {
-        setLoading(true);
+        this.toggleFavoriteVideos = this.toggleFavoriteVideos.bind(this);
+    };
+
+    async fetchLessons(){
+        this.setState({ loading: true });
 
         try {
             const response = await lessons.get();
-            setOriginalList(response);
-            setLessonsList(response);
-        } catch(err) {
+            this.setState({ lessonsList: response, originalList: response});
+        } catch(err){
             console.log(err);
-            setError(err);
+            this.setState({ error: err });
         } finally {
-            setLoading(false);
+            this.setState({ loading: false });
         }
-    }, [setOriginalList, setLoading, setError]);
-
-    // Fetch lessons data when the component mounts
-    useEffect(() => {
-        fetchLessons();
-    }, []);
-
-    // Function to toggle displaying favorite lessons
-    function toggleFavoriteVideos() {
-        const newList = originalList === lessonsList 
-            ? originalList.filter(lesson => lesson.favorite === true) 
-            : originalList;
-        setLessonsList(newList);
     }
 
-    // Function to update the original and lessons list
-    function updateLists(newOriginalList, newLessonsList){
-        setOriginalList(newOriginalList);
-        setLessonsList(newLessonsList);
+    componentDidMount() {
+        this.fetchLessons();
     }
 
-    // Render loading spinner while data is being fetched
-    if (loading) {
+    toggleFavoriteVideos() {
+        const { originalList, lessonsList } = this.state;
+        const newList = originalList === lessonsList ? originalList.filter((lesson) => lesson.favorite === true) : originalList;
+        this.setState({ lessonsList: newList });
+    }
+
+    updateLists(newOriginalList, newLessonsList) {
+        this.setState({ originalList: newOriginalList, lessonsList: newLessonsList });
+    }
+
+    render(){
+        const backgroundColors = ['rgb(243, 243, 136)', 'rgb(241, 201, 208)', 'rgb(178, 178, 222)', 'rgb(178, 222, 184)'];
+        const { lessonsList, originalList, loading, error } = this.state;
+
+        if (loading) {
+            return (
+                <LoadingContainer>
+                    <DotLoader color={"#123abc"} loading={true} size={50} />
+                </LoadingContainer>
+            );
+        } 
+        if (error) {
+            return (
+                <ErrorContainer>
+                    <ErrorBox>
+                        <ErrorImg><img src={sad} alt="Error" /></ErrorImg>
+                        <ErrorText>
+                            <h2>OOPS...</h2>
+                            <p>Something went wrong</p>
+                        </ErrorText>
+                    </ErrorBox>
+                </ErrorContainer>
+            )
+        }
         return (
-            <LoadingContainer>
-                <DotLoader color={"#123abc"} loading={true} size={50} />
-            </LoadingContainer>
+            <>
+                <FilterBar>
+                    <FilterButt onClick={this.toggleFavoriteVideos}>Show favorite lessons</FilterButt>
+                </FilterBar>
+                <Cards>
+                    {this.state.lessonsList.map((lesson, index) => (
+                        <Card 
+                        key={lesson.id} 
+                        lesson={lesson} 
+                        color={backgroundColors[index % backgroundColors.length]}
+                        updateListsFunc={(newOriginalList, newLessonsList) => this.updateLists(newOriginalList, newLessonsList)}                        
+                        originalList={originalList}
+                        lessonsList={lessonsList}
+                        />
+                    ))}
+                </Cards>
+            </>
         );
-    } 
-
-    // Render an error message if there's an error
-    if (error) {
-        return (
-            <ErrorContainer>
-                <ErrorBox>
-                    <ErrorImg><img src={sad} alt="Error" /></ErrorImg>
-                    <ErrorText>
-                        <h2>OOPS...</h2>
-                        <p>Something went wrong</p>
-                    </ErrorText>
-                </ErrorBox>
-            </ErrorContainer>
-        )
-    };
-
-    // Render the main content with filter button and cards
-    return (
-        <>
-            <FilterBar>
-                <FilterButt onClick={() => (toggleFavoriteVideos())}>Show favorite lessons</FilterButt>
-            </FilterBar>
-            <Cards>
-                {lessonsList.map((lesson, index) => (
-                    <Card 
-                    key={lesson.id} 
-                    lesson={lesson} 
-                    color={backgroundColors[index % backgroundColors.length]}
-                    updateListsFunc={updateLists}
-                    originalList={originalList}
-                    lessonsList={lessonsList}
-                    />
-                ))}
-            </Cards>
-        </>
-    );
+    }
 }
